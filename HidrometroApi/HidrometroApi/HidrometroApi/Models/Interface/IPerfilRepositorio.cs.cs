2c318b2b3ef4901acac87b1af99f50184689527b
Jéssica Lucas <jessical.veloso@gmail.com>
2018-11-24 02:03:55 -0200

    Incluindo Repositórios

---
+++ HidrometroApi/HidrometroApi/HidrometroApi/Models/Interface/IPerfilRepositorio.cs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HidrometroApi.Models.Interface
+{
+    public class IPerfilRepositorio
+    {
+    }
+}
---
+++ HidrometroApi/HidrometroApi/HidrometroApi/Models/Repositorio/TaxasRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HidrometroApi.Models.Repositorio
+{
+    public class TaxasRepositorio
+    {
+    }
+}
